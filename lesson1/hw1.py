e988fc7986e3c0b56babac06b5abbb347f88bd89
unknown <you@example.com>
2020-12-27 20:43:49 +0200

    home work 1

--- lesson1/hw1.py
+++ lesson1/hw1.py
@@ -1,1 +1,3 @@
-# Task1
+name = input('Enter your name please')
+hello_str = f'Hello, {name}!'
+print (hello_str)
--- lesson1/hw2.py
+++ lesson1/hw2.py
@@ -1,1 +1,13 @@
-# Task2
+a = input ('Please enter a')
+b = input ('Please enter b')
+c = input ('Please enter c')
+a = int(a)
+b = int(b)
+c = int(c)
+D = b**2 - 4*a*c
+x1 = (-b + D*0.5) / 2*a
+x2 = (-b - D*0.5) / 2*a
+x1text = f"x1={x1}"
+x2text = f"x2={x2}"
+print (x1text)
+print (x2text)
